7782cc21ba3a3d2d2b667f83e8cc3063d7fb1bb5
lozzoc <reach.maddaloc@gmail.com>
2021-09-30 09:52:12 -0400

    added better analytics. ready to dockerize

--- client/index.js
+++ client/index.js
@@ -1,10 +1,15 @@
 const axios = require("axios");
+const cron = require("node-cron")
+const os = require("os")
+const {foobar} = require("../helpers")
+const localip  = foobar("http","0.0.0.0",PORT).localUrlForTerminal;
+
 
 const ARCHAEIC_URL = process.env.SERVER_URL;
 const PORT = process.env.SERVER_PORT
 const AUTH_TOKEN = 'bearer'
-const cron = require("node-cron")
-const {foobar} = require("../helpers")
+
+
 axios.defaults.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
@@ -14,17 +19,32 @@ console.log("NODE SPINNING UP");
 
 cron.schedule('*/1 * * * *', function(){
   console.log("FIRING ANALYTICS");
+  let cpus = {}
+  os.cpus().forEach(function(cpu){
+    if( cpus[`${cpu.model}`] ){
+        cpus[`${cpu.model}`].count += 1;
+    }
+    else{
+       cpus[`${cpu.model}`] = {
+         count: 1,
+         speed: `${cpu.speed} Mhz`,
+       }
+    }
+  })
   axios({
     method: 'post',
     url: `http://${ARCHAEIC_URL}:${PORT}/analytics/uptime`,
     data: {
-      ip: {...foobar("http","0.0.0.0",PORT)},
+      cpus,
+      'osName': `${os.type()}/${os.platform()}`,
+      load: os.loadavg(),
+      ip: localip,
       'process-uptime': process.uptime(),
       'os-uptime': require('os').uptime(),
     }}
   )
   .then(function(){
-
+    return
   })
   .catch(function(e){
     console.log(PORT,ARCHAEIC_URL);
